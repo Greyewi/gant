f864c94bea178fa6b31d5a3a75df478a4c6460ac
unknown <slava-sort@rambler.ru>
2021-04-03 08:53:39 +0300

    package.json

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import React from "react";
+import Month from "./components/Month";
+import TimeContainer from "./components/TimeContainer";
+import Button from "./components/Buttons";
+import {addNextMonth, addPreviousMonth} from "./utils";
+
+
+function App() {
+
+    return (
+        <div className="App">
+            <TimeContainer>
+                <Month />
+            </TimeContainer>
+            <div>
+                <Button onClick={addPreviousMonth} hotKey="left">Prev</Button>
+                <Button onClick={addNextMonth} hotKey='right'>Next</Button>
+            </div>
+        </div>
+    );
+}
+
+export default App;
